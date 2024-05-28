9a0cf11668bbcf8e2df1391fce67d2ac09e4d410
ajea <smithajea@rocketmail.com>
2024-05-28 03:57:50 -0400

    removed protected route middleware

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,6 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isPrivateRoute = createRouteMatcher(["/", "/api/webhook/clerk"]);
-
-export default clerkMiddleware((auth, request) => {
-	if (isPrivateRoute(request)) {
-		auth().protect();
-	}
-});
+export default clerkMiddleware();
 
 export const config = {
 	matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
